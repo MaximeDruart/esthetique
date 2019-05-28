a9367186d2408a0095b0232b4e8d862ca9039158
Maxime Druart <maxime.druart@yahoo.fr>
2019-05-28 16:55:53 +0200

    v0.1 mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
-function dupCheck(array) {
-  let arr2 = []
-  for (item of array) {
-    !arr2.includes(item) ? arr2.push(item) : ""
-  }
-  return arr2
-}
-
-// let arr = [2, 3, 2, 3]
-// arr = dupCheck(arr)
+let menu = document.querySelector('.menu')
+menu.addEventListener('click', (event) => {
+  TweenMax.set('.sandwich-overlay', {display:'block'})
+  TweenMax.to('.sandwich-overlay', 0.6, {opacity:1})
+  console.log('click')
+})
