eeafc11fef0d74a23a802d64c5de1aa39ee31409
Maxime Druart <maxime.druart@yahoo.fr>
2019-06-08 17:34:16 +0200

    rectifs

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector('.menu')
-menu.addEventListener('click', (event) => {
-  TweenMax.set('.sandwich-overlay', {display:'block'})
-  TweenMax.to('.sandwich-overlay', 0.6, {opacity:1})
-  console.log('click')
-})
