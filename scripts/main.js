75630352c9e783ab296a8627257e855294ab9efe
Maxime Druart <maxime.druart@yahoo.fr>
2019-06-16 20:54:06 +0200

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,6 @@
-let buttons = document.querySelector('.link-no-deco')
+let buttons = document.querySelectorAll('.link-no-deco')
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     itemGen(button.getAttribute('data-service'))
   })
 })
-
-function itemGen(attr) {
-  let cont = document.querySelector('services-container')
-  let div = document.createElement('div')
-  div.setAttribute('include-html', attr)
-  cont.appendChild(div)
-}
