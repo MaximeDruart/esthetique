d70d68742f24b961b85b47238550731b16ecf24e
Maxime Druart <maxime.druart@yahoo.fr>
2019-06-16 20:31:34 +0200

    js bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-let buttons = document.querySelector('.link-no-deco')
+let buttons = document.querySelectorAll('.link-no-deco')
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     itemGen(button.getAttribute('data-service'))
