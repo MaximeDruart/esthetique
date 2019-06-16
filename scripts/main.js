205b7d020526b625ca47ce8bced26eda06647b4d
Maxime Druart <maxime.druart@yahoo.fr>
2019-06-16 20:30:10 +0200

    include.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
-class ItemFilter {
-  constructor() {
-    this.classList =
-  }
+let buttons = document.querySelector('.link-no-deco')
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    itemGen(button.getAttribute('data-service'))
+  })
+})
+
+function itemGen(attr) {
+  let cont = document.querySelector('services-container')
+  let div = document.createElement('div')
+  div.setAttribute('include-html', attr)
+  cont.appendChild(div)
 }
