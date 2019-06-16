305a5f9f481749dc1d204a8d9748b8425333f909
LucienChotard <chotardlucien@gmail.com>
2019-06-16 22:05:47 +0200

    alignement

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-let buttons = document.querySelectorAll('.link-no-deco')
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    $("#services-container").load("include/"+button.getAttribute('data-service')+".html");
-  })
-})
