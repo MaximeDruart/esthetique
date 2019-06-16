e615bf084b9a84962fa2de9a3020aab16ed124fc
LucienChotard <chotardlucien@gmail.com>
2019-06-16 22:12:27 +0200

    Revert "alignement"
    This reverts commit 305a5f9f481749dc1d204a8d9748b8425333f909.

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let buttons = document.querySelectorAll('.link-no-deco')
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    $("#services-container").load("include/"+button.getAttribute('data-service')+".html");
+  })
+})
