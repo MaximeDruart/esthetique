1ede81eb272198660680762241c2e4ef35a2c03f
LucienChotard <chotardlucien@gmail.com>
2019-06-17 21:06:56 +0200

    dropdown menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
+// Default cat
+$("#services-container").load("include/corps.html");
+
+// Filter services
 let buttons = document.querySelectorAll('.link-no-deco')
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     $("#services-container").load("include/"+button.getAttribute('data-service')+".html");
   })
+})
+
+// Dropdown
+let navTrigger = document.querySelector('#navTrigger')
+let navDropdown = document.querySelector('#navDropdown')
+navTrigger.addEventListener('click', () => {
+  navDropdown.classList.toggle("dropdown_active")
 })
