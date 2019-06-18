a1579f3ef1369762329cb330f6af669be88af9cf
Maxime Druart <maxime.druart@yahoo.fr>
2019-06-19 00:17:57 +0200

    header responsive

--- scripts/main.js
+++ scripts/main.js
@@ -9,11 +9,11 @@ window.onload = function(){
 let buttons = document.querySelectorAll('li a.link-no-deco')
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    buttons.forEach(button =>{
+    buttons.forEach(button => {
       button.classList.remove('active')
     })
     button.classList.add('active')
-
+    overlay.style.display == "block" ? (tlOverlay.reverse(), hamburger.classList.toggle('is-active')) : ""
     TweenMax.set('#services-container', {opacity:0})
     $("#services-container").load("include/"+button.getAttribute('data-service')+".html");
     TweenMax.to('#services-container', 0.6, {opacity:1})
@@ -33,7 +33,13 @@ buttons.forEach((button) => {
 //   }
 // })
 
+
+let overlay = document.querySelector('.overlay')
 let hamburger = document.querySelector('.hamburger')
-hamburger.addEventListener('click', () => {
+let tlOverlay = new TimelineMax({paused:true})
+tlOverlay.set(overlay, {display:"block"})
+tlOverlay.to(overlay, 0.3, {ease:"easeInOut", opacity:1})
+hamburger.addEventListener('click', (e) => {
   hamburger.classList.toggle('is-active')
+  hamburger.classList.contains('is-active') ? tlOverlay.play() : tlOverlay.reverse()
 })
