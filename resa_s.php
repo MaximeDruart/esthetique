04da37d4661eeb6bb0fedf6629bf13ef3392badc
LucienChotard <chotardlucien@gmail.com>
2019-06-20 21:29:43 +0200

    resa php update

--- resa_s.php
+++ resa_s.php
@@ -1,17 +1,19 @@
 <?php
-$to      = 'showroomrelookingms@gmail.com';
-$subject = 'Showroomrelooking RDV - A '.htmlspecialchars($_POST['hour']).' le '.htmlspecialchars($_POST['date']).' pour '.htmlspecialchars($_POST['service']);
 
-$message = 'Date :'.htmlspecialchars($_POST['date'])."\n".
-'Horaire :'.htmlspecialchars($_POST['hour'])."\n".
-'Service :'.htmlspecialchars($_POST['service'])."\n".
-'Nom / prénom :'.htmlspecialchars($_POST['hour'])."\n".
-'Infos supplémentaires :'.htmlspecialchars($_POST['message'])."\n";
+$date = date("d-m-Y", strtotime($_POST['date']));
+$to      = 'showroomrelookingms@gmail.com';
+$subject = 'Showroomrelooking RDV - A '.htmlspecialchars($_POST['hour']).' le '.$date.' pour '.htmlspecialchars($_POST['service']);
+
+$message = 'Date : '.htmlspecialchars($date)."\n \n".
+'Horaire : '.htmlspecialchars($_POST['hour'])."\n \n".
+'Service : '.htmlspecialchars($_POST['service'])."\n \n".
+'Nom / prénom : '.htmlspecialchars($_POST['name'])."\n \n".
+'Infos supplémentaires : '.htmlspecialchars($_POST['message'])."\n \n";
 
 $headers = 'From: showroomrelookingms@gmail.com' . "\r\n" .
     'Reply-To: showroomrelookingms@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
+    'X-Mailer: PHP/' . phpversion(). "\r\n";
+    
 mail($to, $subject, $message, $headers);
-header("Refresh: 6;url=showroomrelooking.fr/");
+header("Refresh: 6;url=/");
 ?>
